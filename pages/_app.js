90e837029f5d4aa7b9f7bb4bdf196d85fbec7a88
Manoj Mukherjee <Manoj.Mukherjee@optiv.com>
2020-03-22 20:51:13 +0530

    fab icon

--- pages/_app.js
+++ pages/_app.js
@@ -33,7 +33,6 @@ export default class MyApp extends App {
           <meta name="twitter:description" content="covid19 India deails data" />
           <meta name="twitter:site" content="@MoHFW_INDIA" />
           <meta name="twitter:creator" content="ManojMukherje19" />
-          <link rel="manifest" href="/manifest.json" />
           <title>COVID-19 India Data</title>
           <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
 
@@ -56,6 +55,23 @@ export default class MyApp extends App {
           <meta name="layoutmode" content="fitscreen/standard" />
           <meta name="imagemode" content="force" />
           <meta name="screen-orientation" content="portrait" />
+          <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
+          <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
+          <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
+          <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
+          <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
+          <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png" />
+          <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
+          <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
+          <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/manifest.json" />
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <ThemeProvider theme={theme}>
           {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
